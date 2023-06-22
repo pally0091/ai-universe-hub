f825a3ddb093c08844e8f71ac082d1269e404b90
Debashis Roy <goodquote39@hotmail.com>
2023-06-23 03:51:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>AI Universe Hub</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
