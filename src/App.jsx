8f9a16a43d5aae0e8f8f8acd7ae469dc938fa1c5
Debashis Roy <goodquote39@hotmail.com>
2023-06-27 01:46:01 +0600

    api added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./App.css";
 
 function App() {
+  const [tools, setTools] = useState();
+
+  useEffect(() => {
+    fetch("https://openapi.programming-hero.com/api/ai/tools")
+      .then((res) => res.json())
+      .then((data) => setTools(data?.data.tools));
+    console.log(tools);
+  }, []);
   return (
     <>
       <div>
         <h1
           id="head-name"
-          className="text-5xl font-bold bg-black text-gray-200 py-4 italic"
+          className="text-5xl font-bold bg-black text-gray-600 py-4 italic"
         >
           AI Universe Hub
         </h1>
         <div>items </div>
+        <footer>footer section</footer>
       </div>
     </>
   );
